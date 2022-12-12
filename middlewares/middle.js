3a5d062dc6bdfb5277c9dd45d58bcd88a68afbf5
sourabh1392 <114735918+sourabh1392@users.noreply.github.com>
2022-12-13 00:31:34 +0530

    Update middle.js

--- middlewares/middle.js
+++ middlewares/middle.js
@@ -4,20 +4,23 @@ const authenticate = function (req, res, next) {
     try {
         const header = req.headers["x-api-key"]
         if (header) {
-            const verify = jwt.verify(header, "pass123")
-            if (verify) {
+            const verify = jwt.verify(header, "pass123",(error,verify)=>{
+                if (error) {
+                    if(error.message == "jwt expired"){
+                        return res.status(400).send({status:false, message:"Your token is expired"})
+                    }
+                    else return res.status(401).send({status:false,message:"Authentication failed"})
+                }
                 req.verify = verify
                 next()
-            }
-            else {
-                return res.status(401).send({ status: false, msg: "Invalid token" })
-            }
+            })
         }
-        else return res.status(401).send("Token Is Missing")
+        else return res.status(401).send({status:false,message:"Token is missing"})
     }
     catch (err) {
         res.status(500).send({status:false,message:err.message})
     }
 }
+
 
 module.exports.authenticate = authenticate
